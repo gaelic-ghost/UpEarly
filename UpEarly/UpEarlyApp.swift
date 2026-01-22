d33413ca946f9f855e29e51ca2d0449e6828237f
Gale <236288055+gaelic-ghost@users.noreply.github.com>
2026-01-22 05:46:15 -0500

    Initial Commit

---
+++ UpEarly/UpEarlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpEarlyApp.swift
+//  UpEarly
+//
+//  Created by Gale Williams on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct UpEarlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
